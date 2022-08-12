f9d2fbc88630b396a002d04558b794ed165a64ab
Zokky2e <icro4.gaming@hotmail.com>
2022-08-12 11:56:28 +0200

    bugfixed starting prices not match prices on display

--- src/modules/car-select/components/car-card/CarCard.tsx
+++ src/modules/car-select/components/car-card/CarCard.tsx
@@ -1,4 +1,6 @@
 /** @jsxImportSource @emotion/react */
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useResetRecoilState, useSetRecoilState } from "recoil";
 import { sharedAtoms } from "../../../../shared";
@@ -6,8 +8,11 @@ import { configurationViewAtoms } from "../../../configuration-view";
 import { configuratorAtoms } from "../../../configurator";
 import { CarCardInfo } from "../../types";
 import { styles } from "./CarCard.styles";
+import loadingImage from "../../assets/front-1-rs5.png";
 export function CarCard(props: CarCardInfo) {
   const navigate = useNavigate();
+  const storage = getStorage();
+  const gsReference = ref(storage, props.picture);
   const setName = useSetRecoilState(configurationViewAtoms.name);
   const resetId = useResetRecoilState(configurationViewAtoms.id);
   const setWheels = useSetRecoilState(configurationViewAtoms.wheels);
@@ -60,9 +65,21 @@ export function CarCard(props: CarCardInfo) {
       });
     }, 500);
   }
+  const [image, setImage] = useState<string>(loadingImage);
+  useEffect(() => {
+    getDownloadURL(gsReference)
+      .then((url) => {
+        setImage(url);
+      })
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gsReference]);
   return (
     <li css={styles.card}>
-      <img css={styles.image} src={props.picture} alt="car" />
+      <img css={styles.image} src={image} alt="car" />
       <p css={styles.year}>{props.year}</p>
       <p css={styles.name}>{props.name}</p>
       <button css={styles.button} onClick={handleRedirect}>
--- src/modules/car-select/components/car-picker/CarPicker.tsx
+++ src/modules/car-select/components/car-picker/CarPicker.tsx
@@ -1,9 +1,56 @@
 /** @jsxImportSource @emotion/react */
 
-import { Cars } from "../../const";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../../../../firebase";
+import { CarCardInfo } from "../../types";
 import { CarCard } from "../car-card";
 import { styles } from "./CarPicker.styles";
 export function CarPicker() {
+  const [Cars, setCars] = useState<CarCardInfo[]>([]);
+  const CarCardInfoConverter = {
+    toFirestore: (car: CarCardInfo) => {
+      return {
+        docName: car.docName,
+        picture: car.picture,
+        year: car.year,
+        name: car.name,
+        colorExterior: car.colorExterior,
+        colorInterior: car.colorInterior,
+      };
+    },
+    fromFirestore: (snapshot: { data: () => any }) => {
+      const data = snapshot.data();
+      const item: CarCardInfo = {
+        docName: data.docName,
+        picture: data.picture,
+        year: data.year,
+        name: data.name,
+        colorExterior: data.colorExterior,
+        colorInterior: data.colorInterior,
+      };
+      return item;
+    },
+  };
+  async function fetchCarSelectList() {
+    const q = query(collection(db, "car-select-list"));
+    onSnapshot(q, (querySnapshot) => {
+      if (querySnapshot.empty) {
+        return;
+      }
+      const docsItems: CarCardInfo[] = [];
+      querySnapshot.docs.forEach((value) => {
+        const item = CarCardInfoConverter.fromFirestore(value);
+        docsItems.push(item);
+      });
+      setCars(docsItems);
+    });
+  }
+  useEffect(() => {
+    fetchCarSelectList();
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <section>
       <h2 css={styles.title}>Configure a car</h2>
